91d1b0b7e7fafabc6f682d01b369ad5ebbcfc4d2
Kit <46611409+ankitbaraskar@users.noreply.github.com>
2019-06-22 16:38:34 -0400

    added components' js files and render them to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from "react";
+import Nav from "./components/js/nav";
+import Header from "./components/js/header";
+import Main from "./components/js/main";
+import Footer from "./components/js/footer";
 
 class App extends Component{
     render(){
         return (
             <div>
-                hi
+                <Nav />
+                <Header />
+                <Main />
+                <Footer />
             </div>
         )
     }
